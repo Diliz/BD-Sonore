66733704f3fbc56cf82561d8e8b6edabc513f08c
Diliz <jtoubert@student.42.fr>
2015-11-06 00:03:35 +0100

    add comments

--- www/js/app.js
+++ www/js/app.js
@@ -8,15 +8,16 @@ angular.module('test', ['ionic'])
   };
 
   var datas = {
-    nbrOfImages: 7,
-    nbrOfAmbSounds: 4,
-    nbrOfPonctSounds: 2,
-    ambSounds: [],
-    ponctSounds: []
+    nbrOfImages: 7, // set le nombre d'images ici, je n'ai pas pris le temps d'aller chercher les images dans un dossier que tu aurai pu spécifier
+    nbrOfAmbSounds: 4, // le nombre de sons ambiants
+    nbrOfPonctSounds: 2, // le nombre de sons ponctuels
+    ambSounds: [], // tableau contenant les sons ambiants (générés par initAmbSounds)
+    ponctSounds: [] // tableau contenant les sons ponctuels (générés par initPonctSounds)
   }
 
   soundManager.setup({
     onready: function() {
+      // une fois le sound manager prêt on initialise les sons
       initAmbSounds();
       initPonctSounds();
       console.log("Sounds Ready");
@@ -28,12 +29,17 @@ angular.module('test', ['ionic'])
 
   $scope.getScrollPosition = function(){
       console.log("Scroll pos from top: ", $ionicScrollDelegate.getScrollPosition().top);
-
+      // ici la fonction playsoundatposy contient la position y en % de l'endroit ou tu veux jouer le son,
+      // avec une marge d'erreur posVar en % en fonction de la taille d'une image
+      // quel son doit on jouer avec which sound, et le numéro de l'image sur laquelle on se positionne
       playSoungAtPosY(25, {type: "amb", whichImg: 2, whichSound: 2, posVar: 10, playing: false});
   }
 
+  // initialise les images, les sons de ta bd
+
   function initImgs(){
     for(var i = 1; i <= datas.nbrOfImages; i++) {
+      // génère les images dans l'index.html
       $scope.data.items.push({src: "img/bd/Case" + i + ".jpg"});
     }
   }
@@ -58,6 +64,8 @@ angular.module('test', ['ionic'])
     }
   }
 
+  // joue un son prédéfinie à une hauteur prédéfinie en % sur une image prédéfinie
+  // avec une marge de positionnement définie en %
   function playSoungAtPosY(y, sound){
       var posY = (($('.scroll').height() / datas.nbrOfImages) / 100) * sound.whichImg * y;
       var posVar = (($('.scroll').height() / datas.nbrOfImages) / 100) * sound.posVar;
@@ -72,13 +80,15 @@ angular.module('test', ['ionic'])
       console.log("pos: " + position + "   posY: " + posY + "   jeu: " + posVar);
   }
 
+  // play et stop le son, simplement
+
   function LoopSound(sound, playStop, volume){
     if (sound.type == "amb") {
       if (playStop == "play") {
         datas.ambSounds[(sound.whichSound - 1)].play();
         return true;
       } else {
-        datas.ambSounds[(sound.whichSound - 1)].stop;
+        datas.ambSounds[(sound.whichSound - 1)].stop();
         return false;
       }
     }
@@ -87,7 +97,7 @@ angular.module('test', ['ionic'])
         datas.ponctSounds[(sound.whichSound - 1)].play();
         return true;
       } else {
-        datas.ponctSounds[(sound.whichSound - 1)].stop;
+        datas.ponctSounds[(sound.whichSound - 1)].stop();
         return false;
       }
     }
